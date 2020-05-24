074b1e77cb62a9d6fe84c8bb3f91cd0e6f377fc5
novak254 <ndemokelvinonkundi@gmail.com>
2020-05-24 17:12:35 +0300

    row for cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,27 +10,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-      ),
+      backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 10),
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 80,
-                child: CircleAvatar(
-                  radius: 30.0,
-                  backgroundColor: Colors.blue,
-                  backgroundImage:
-                      NetworkImage("https://via.placeholder.com/150"),
-                ),
-              )
-            ],
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+              radius: 50, backgroundImage: AssetImage('images/lec.jpg')),
+          Text(
+            "Kelvin Onkundi",
+            style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold),
           ),
-        ),
-      ),
+          Text(
+            "Software Engineer".toUpperCase(),
+            style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 18,
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold),
+          ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "+254 799 935 239",
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                      fontFamily: "SourceSansPro"),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("ndemokelvinonkundi@gmail.com")
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     ));
   }
 }
