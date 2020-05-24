c386a2c616a274f452b862fc641aa1d966dbde57
novak254 <ndemokelvinonkundi@gmail.com>
2020-05-24 13:35:33 +0300

    whatsapp

--- lib/main.dart
+++ lib/main.dart
@@ -8,59 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("Novak"),
-        ),
-        backgroundColor: Colors.green,
-        body: SafeArea(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+      ),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(top: 10),
           child: Row(
-//            mainAxisSize: MainAxisSize.min,
-//            verticalDirection: VerticalDirection.up,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.orange,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                    )
-                  ],
+                height: 80,
+                child: CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: Colors.blue,
+                  backgroundImage:
+                      NetworkImage("https://via.placeholder.com/150"),
                 ),
-              ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
-                child: Text("Container 2"),
               )
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          child: Icon(Icons.add),
-        ),
       ),
-    );
+    ));
   }
 }
